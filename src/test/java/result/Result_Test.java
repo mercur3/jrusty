c160162d14e46735f830f03299a5b497befc227b
Andri Reveli <reveliandri@gmail.com>
2021-10-02 18:29:34 +0200

    reformat code

--- src/test/java/result/Result_Test.java
+++ src/test/java/result/Result_Test.java
@@ -7,85 +7,83 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Result_Test {
-    @Test
-    void ok_path_works() {
-        var str = "Hello, world!";
-        var x = new Ok<String, Integer>(str);
+	private static final String ERROR_MESSAGE = "It cannot be parsed";
 
-        assertTrue(x.isOk());
-        assertFalse(x.isErr());
-        assertEquals(x.ok(), Optional.of(str));
-        assertEquals(x.err(), Optional.empty());
-        assertEquals(x.unwrap(), str);
-        assertEquals(x.unwrapOr(""), str);
-        assertEquals(x.unwrapOrElse(Result_Test::__toString), str);
-    }
+	private static Result<Integer, String> __parseInt(String str) {
+		try {
+			return new Ok<>(Integer.parseInt(str));
+		}
+		catch (NumberFormatException e) {
+			return new Err<>(ERROR_MESSAGE);
+		}
+	}
 
-    @Test
-    void err_path_works() {
-        var str = "Some error type";
-        var x = new Err<String, Integer>(str);
+	private static int __length(String str) {
+		return str.length();
+	}
 
-        assertFalse(x.isOk());
-        assertTrue(x.isErr());
-        assertEquals(x.ok(), Optional.empty());
-        assertEquals(x.err(), Optional.of(str));
-        assertThrows(IllegalStateException.class, x::unwrap);
-        assertEquals(x.unwrapOr(1), 1);
-        assertEquals(x.unwrapOrElse(Result_Test::__length), str.length());
-    }
-
-    @Test
-    void function_works() {
-        int num = 1234;
-        var ok = __parseInt("1234");
-        var err = __parseInt("LOL");
-
-        assertTrue(ok.isOk());
-        assertFalse(ok.isErr());
-        assertEquals(ok.ok(), Optional.of(num));
-        assertEquals(ok.err(), Optional.empty());
-        assertEquals(ok.unwrap(), num);
-        assertEquals(ok.unwrapOr(0), num);
-        assertEquals(ok.unwrapOrElse(Result_Test::__length), num);
-
-        assertFalse(err.isOk());
-        assertTrue(err.isErr());
-        assertEquals(err.ok(), Optional.empty());
-        assertEquals(err.err(), Optional.of("It cannot be parsed"));
-        assertThrows(IllegalStateException.class, err::unwrap);
-        assertEquals(err.unwrapOr(1), 1);
-        assertEquals(err.unwrapOrElse(Result_Test::__length), ERROR_MESSAGE.length());
-    }
-
-    @Test
-    void null_not_allowed() {
-        assertThrows(NullPointerException.class, () -> new Ok<>(null));
-        assertThrows(NullPointerException.class, () -> new Err<>(null));
-    }
+	private static String __toString(int x) {
+		return String.valueOf(x);
+	}
 
 
 
-    /*     PRIVATE     */
+	/*     PRIVATE     */
 
+	@Test
+	void ok_path_works() {
+		var str = "Hello, world!";
+		var x = new Ok<String, Integer>(str);
 
+		assertTrue(x.isOk());
+		assertFalse(x.isErr());
+		assertEquals(x.ok(), Optional.of(str));
+		assertEquals(x.err(), Optional.empty());
+		assertEquals(x.unwrap(), str);
+		assertEquals(x.unwrapOr(""), str);
+		assertEquals(x.unwrapOrElse(Result_Test::__toString), str);
+	}
 
-    private static final String ERROR_MESSAGE = "It cannot be parsed";
+	@Test
+	void err_path_works() {
+		var str = "Some error type";
+		var x = new Err<String, Integer>(str);
 
-    private static Result<Integer, String> __parseInt(String str) {
-        try {
-            return new Ok<>(Integer.parseInt(str));
-        }
-        catch (NumberFormatException e) {
-            return new Err<>(ERROR_MESSAGE);
-        }
-    }
+		assertFalse(x.isOk());
+		assertTrue(x.isErr());
+		assertEquals(x.ok(), Optional.empty());
+		assertEquals(x.err(), Optional.of(str));
+		assertThrows(IllegalStateException.class, x::unwrap);
+		assertEquals(x.unwrapOr(1), 1);
+		assertEquals(x.unwrapOrElse(Result_Test::__length), str.length());
+	}
 
-    private static int __length(String str) {
-        return str.length();
-    }
+	@Test
+	void function_works() {
+		int num = 1234;
+		var ok = __parseInt("1234");
+		var err = __parseInt("LOL");
 
-    private static String __toString(int x) {
-        return String.valueOf(x);
-    }
+		assertTrue(ok.isOk());
+		assertFalse(ok.isErr());
+		assertEquals(ok.ok(), Optional.of(num));
+		assertEquals(ok.err(), Optional.empty());
+		assertEquals(ok.unwrap(), num);
+		assertEquals(ok.unwrapOr(0), num);
+		assertEquals(ok.unwrapOrElse(Result_Test::__length), num);
+
+		assertFalse(err.isOk());
+		assertTrue(err.isErr());
+		assertEquals(err.ok(), Optional.empty());
+		assertEquals(err.err(), Optional.of("It cannot be parsed"));
+		assertThrows(IllegalStateException.class, err::unwrap);
+		assertEquals(err.unwrapOr(1), 1);
+		assertEquals(err.unwrapOrElse(Result_Test::__length), ERROR_MESSAGE.length());
+	}
+
+	@Test
+	void null_not_allowed() {
+		assertThrows(NullPointerException.class, () -> new Ok<>(null));
+		assertThrows(NullPointerException.class, () -> new Err<>(null));
+	}
 }
