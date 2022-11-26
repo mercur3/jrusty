a532a673c272d6727213a564581b90a4133248f9
Andri Reveli <reveliandri@gmail.com>
2022-11-26 10:06:18 +0100

    Annotate src blocks

--- git-push.sh
+++ git-push.sh
@@ -5,5 +5,6 @@ cd $(dirname $0)
 
 git push origin --all
 git push origin --tags
-git push --mirror https://gitlab.com/mercur3/jrusty.git
+git push https://gitlab.com/mercur3/jrusty.git --all
+git push https://gitlab.com/mercur3/jrusty.git --tags
 
